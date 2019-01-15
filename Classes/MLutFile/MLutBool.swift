974151d1b0af0027cc4e50f609c9f17c94858f13
denn nevera (mbp17) <denn.nevera@gmail.com>
2019-01-15 15:16:38 +0300

    rollback bool value

--- Classes/MLutFile/MLutBool.swift
+++ Classes/MLutFile/MLutBool.swift
@@ -11,11 +11,6 @@ import IMProcessingXMP
 
 public extension MLutBoolModel {
     
-    public convenience init(meta: ImageMeta) throws {
-        self.init()
-        try create(meta: meta)
-    }
-    
     public var state:Bool {
         get {
             return nsstate.boolValue
@@ -23,11 +18,33 @@ public extension MLutBoolModel {
         set {
             nsstate = NSNumber(value: newValue)
         }
+    }  
+}
+
+
+public extension MLutPrinted {
+    public convenience init(meta: ImageMeta) throws {
+        self.init()
+        do {
+            let t = try meta.getField(MLutPrinted.self, fieldId: nil) as! MLutBoolModel
+            state = t.nsstate.boolValue
+        }
+        catch {
+            state = false
+        }
     }
-    
-    private func create<T:MLutBoolModel>(meta: ImageMeta) throws -> T {
-        let t = try meta.getField(T.self, fieldId: nil) as! T
-        state = t.nsstate.boolValue
-        return t
+}
+
+
+public extension MLutPublish {
+    public convenience init(meta: ImageMeta) throws {
+        self.init()
+        do {
+            let t = try meta.getField(MLutPublish.self, fieldId: nil) as! MLutBoolModel
+            state = t.nsstate.boolValue
+        }
+        catch {
+            state = false
+        }
     }
 }
--- test/MLutFile-Pod/MLutFile-Pod/AppDelegate.swift
+++ test/MLutFile-Pod/MLutFile-Pod/AppDelegate.swift
@@ -18,11 +18,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     let cap = MLutColorType.color.caption
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
+        
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
     }
 
 
